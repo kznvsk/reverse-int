ef94c2be498b2daf9155b38bc80e46cf8fa5eec5
kznvsk <olga_kazanovskaya@mail.ru>
2022-08-11 23:44:42 +0700

    edit file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (Number(n) >= 0) {
+        let str = String(n);
+        return str.split("").reverse().join("");
+    } else {
+        let num = Number(n) * -1;
+        let str = String(num);
+        return str.split("").reverse().join("");
+    }
 }
